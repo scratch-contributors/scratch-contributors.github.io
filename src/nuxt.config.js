61c400ac4ba40984c3260c806801a531aa1e8ea4
FunctionalMetatable <>
2021-05-30 19:05:16 +0000

    lets see if i can do nuxt xD

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', href: '/icon.png' }
+      { rel: 'icon', href: 'icon.png' }
     ]
   },
 
---
+++ src/pages/contributors.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 style="color: black;">Contributors</h1>
+</template>
+<script>
+  export default {
+    title: 'Meet the contributors',
+    async asyncData({ req, isServer, params, store }) {
+
+    }
+  }
+</script>
