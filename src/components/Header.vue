65ebd313ad903268f2708976cf0712dfb28fa579
FunctionalMetatable <>
2021-05-31 08:51:02 +0000

    test

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,3 +13,54 @@
     </div>
   </div>
 </template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+h1, h2, h3, h4, h5, h6, a, p :not(.nav-item *) {
+  color: black;
+}
+
+.nav .nav-item:not(.img) {
+  background-color: #4D97FF;
+  color: white;
+  height: 65px;
+  display: block;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  text-size: 55px;
+  padding: 22px 16px;
+  float: left;
+}
+
+.nav {
+  background-color: #4D97FF;
+  margin: 0;
+  list-style-type: none;
+  padding: 0;
+  overflow: hidden;
+  height: 65px;
+}
+
+.nav-item :hover {
+  background-color: #4588E5;
+}
+
+.nav-item NuxtLink :hover {
+  background-color: #4588E5;
+}
+
+.nav .nav-item.img {
+  display: block;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  text-size: 55px;
+  padding: 0px 0px;
+  float: left;
+  background-color: #151618;
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <Nuxt />
+  <div>
+    <Nuxt />
+  </div>
 </template>
 
 <style>
@@ -20,50 +22,5 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-}
-
-h1, h2, h3, h4, h5, h6, a, p :not(.nav-item *) {
-  color: black;
-}
-
-.nav .nav-item:not(.img) {
-  background-color: #4D97FF;
-  color: white;
-  height: 65px;
-  display: block;
-  color: white;
-  text-align: center;
-  text-decoration: none;
-  text-size: 55px;
-  padding: 22px 16px;
-  float: left;
-}
-
-.nav {
-  background-color: #4D97FF;
-  margin: 0;
-  list-style-type: none;
-  padding: 0;
-  overflow: hidden;
-  height: 65px;
-}
-
-.nav-item :hover {
-  background-color: #4588E5;
-}
-
-.nav-item NuxtLink :hover {
-  background-color: #4588E5;
-}
-
-.nav .nav-item.img {
-  display: block;
-  color: white;
-  text-align: center;
-  text-decoration: none;
-  text-size: 55px;
-  padding: 0px 0px;
-  float: left;
-  background-color: #151618;
 }
 </style>
