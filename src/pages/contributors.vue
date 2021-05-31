5302525fcaa1f67d5115b2f8bbc0f39f38185cc8
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-05-31 20:22:51 +0100

    stuck in debugging

--- src/pages/contributors.vue
+++ src/pages/contributors.vue
@@ -5,7 +5,7 @@
       <h1 style="color: black;">Contributors</h1>
       <h3>Managers</h3>
       <div>
-        <template v-for="manager in managers" :key="manager.name">
+        <template :v-for="manager in managers" :key="manager.name">
           <User :user="manager.name" :bio="manager.bio" />
           <hr />
         </template>
@@ -15,7 +15,7 @@
       <br />
       <h3>Curators</h3>
       <div>
-        <template v-for="curator in curators" :key="curator.name">
+        <template :v-for="curator in curators" :key="curator.name">
           <User :user="curator.name" :bio="curator.bio" />
           <hr />
         </template>
