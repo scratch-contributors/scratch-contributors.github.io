7c39243d4906d2bf1be645f47df3012cc3534d9b
FunctionalMetatable <>
2021-05-31 10:18:41 +0000

    fix

--- src/components/OcularStatus.vue
+++ src/components/OcularStatus.vue
@@ -1,17 +1,19 @@
 <template>
   <i class="ocular-status"> {{ ocular.status }}
-    <span class="ocular-color" :style="background-color:${ocular.color}"></span>
+    <span class="ocular-color" style="`background-color:${ocular.color}`"></span>
   </i>
 </template>
 <script>
   export default {
     props: ["user"],
     data() {
-      ocular: {status: "", color: ""}
+      return {
+        ocular: {status: "", color: ""}
+      }
     },
     async fetch() {
-      let res = await fetch(`https://my-ocular.jeffalo.net/api/user/${this.user}`)
-      let ocular = await res.json()
+      let res = await fetch(`https://my-ocular.jeffalo.net/api/user/${this.user}`);
+      let ocular = await res.json();
 
       this.ocular = ocular
     },
--- src/components/User.vue
+++ src/components/User.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="user">
     <p>{{ username }}</p>
-    <img :src="`https://cdn2.scratch.mit.edu/get_image/user/${username}_50x50.png`" />
+    <img src="`https://cdn2.scratch.mit.edu/get_image/user/${username}_50x50.png`" />
     <OcularStatus :user="`${username}`" />
   </div>
 </template>
@@ -9,7 +9,9 @@
   export default {
     props: ["user"],
     data() {
-      userid: 0
+      return {
+        userid: 0
+      }
     },
     async fetch() {
       let scratchRes = await fetch(`https://api.scratch.mit.edu/users/${this.user}`);
