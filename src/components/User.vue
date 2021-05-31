c3579bbf11a2f38a643db3faa6eb92338de6c475
FunctionalMetatable <>
2021-05-31 12:16:24 +0000

    style fixes

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="container">
     <header>
-      <p>Joined at {{ joined }}</p>
+      <p>Joined Scratch at {{ joined }}</p>
     </header>
-    <div class="left">
-      <img :src="`https://cdn2.scratch.mit.edu/get_image/user/${userid}_50x50.png`" />
-      <a :href="`https://scratch.mit.edu/users/${username}`">{{ username }}</a>
-      <OcularStatus :user="this.user" />
-    </div>
-    <div class="right">
-      {{ bio }}
+    <div class="wrap">
+      <div class="right">
+        <div class="content">
+          {{ bio }}
+        </div>
+      </div>
+
+      <div class="left">
+        <img :src="`https://cdn2.scratch.mit.edu/get_image/user/${userid}_90x90.png`" class="pfp" />
+        <a :href="`https://scratch.mit.edu/users/${username}`" class="username">{{ username }}</a>
+        <OcularStatus :user="this.user" />
+      </div>
+
     </div>
   </div>
 </template>
@@ -38,7 +44,7 @@
   /* some styling elements taken from ocular, thanks jeffalo */
   header {
     background-color: #4D97FF;
-    border-radius: 50px 50px 0px 0px;
+    border-radius: 10px 10px 0px 0px;
     padding: 10px;
     font-weight: 700;
     white-space: nowrap;
@@ -48,15 +54,48 @@
   .left {
     float: left;
     width: 15%;
+    order: 1;
+    flex: 0 0 auto;
   }
   .right {
     float: right;
     width: 85%;
-    padding: .75em 1em;
     box-shadow: inset 1px 0 #e0e0e0;
     flex: 0 0 auto;
   }
+
+  .main-content, .main-sidebar, .main-nav {
+    padding: 0.75em 1em;
+    overflow-wrap: break-word;
+  }
+
   .container {
     margin-bottom: 20px;
   }
+
+  .pfp {
+    width: 90px;
+    height: 90px;
+  }
+
+  .wrap {
+    display: flex;
+    border-left: none;
+  }
+
+  .right {
+    order: 2;
+    width: 85%;
+    background: var(--background);
+    overflow: hidden;
+    display: flex;
+    flex-flow: row wrap;
+  }
+
+  .content {
+    display: block;
+    width: 100%;
+  }
+
+
 </style>
