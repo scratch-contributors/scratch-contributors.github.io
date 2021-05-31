1e9d87592b592d36b42fb2a8514f3a636008d483
FunctionalMetatable <>
2021-05-31 09:07:35 +0000

    remove redirect-module and tailwind

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -32,10 +32,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
