9cc1b6b5a02242dfaa13b1309ccf46f3631b8ffa
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-06-01 15:00:39 +0000

    ok

--- src/components/User.vue
+++ src/components/User.vue
@@ -33,9 +33,7 @@ export default {
     };
   },
   async fetch() {
-    let scratchRes = await fetch(
-      `https://api.scratch.mit.edu/users/${this.user}`
-    );
+    let scratchRes = await fetch(`https://api.scratch.mit.edu/users/${this.user}`);
     let scratchJson = await scratchRes.json();
     this.userid = scratchJson.id;
     this.username = scratchJson.username;
